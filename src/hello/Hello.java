c8dfdfc17d129505df6692baf3ff10354d819aa2
poojapawar123 <31065860+poojapawar123@users.noreply.github.com>
2017-08-24 04:56:03 -0700

    fifth commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -9,9 +9,10 @@ public class Hello {
 		}
 		int a=8,b=0,c=0;
 		System.out.println("a="+8 +" b="+b);
+		System.out.println("Hello github");
 		c=a/b;
 		System.out.println(c);
-		System.out.println("Hello github");
+		
 	}
 
 }
